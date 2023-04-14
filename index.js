32a201cf6007b9175051170907921ece95525247
JohnJairoLancherosRangel <jjlancherosr@correo.udistrital.edu.co>
2023-04-13 22:39:42 -0500

    Subiendo Página Kenworth de la Montaña Primera Versión

---
+++ index.js
@@ -0,0 +1,6 @@
+var tanque = 40;
+var recorrdioGasolina = 480;
+var recorridoAlcohol = 300;
+
+document.write("La Eficiencia del Carro Usando Gasolina Es: " + (recorrdioGasolina/tanque) + "Km/Litro");
+document.write("La Eficiencia del Carro Usando Alcohol Es: " + (recorrdioAlcohol/tanque) + "Km/Litro");
